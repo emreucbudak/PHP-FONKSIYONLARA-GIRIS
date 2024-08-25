72e513a3bd9cba51df6c89e381a72bbe99aae64a
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-25 22:23:15 +0300

    Add files via upload

---
+++ main (15).php
@@ -0,0 +1,20 @@
+<?php
+/******************************************************************************
+
+Emre Üçbudak Karadeniz Teknik Üniveristesi
+
+*******************************************************************************/
+// PHP FONKSIYONLARA GIRIS
+$ad = 5;
+function azalt($ad) {
+    $ad = 1;
+    echo("Ad 5 iken 1e eşitlenecek fonksiyon sayesinde\n");
+    echo($ad);
+    
+}
+azalt($ad);
+
+
+
+
+?>
